cb72b5838e55bd3386d24cdf1b07abad3038c9a2
tuyennd <tuyennd@rocketstudio.com.vn>
2023-12-06 14:42:39 +0700

    init

---
+++ lib/dice_roller.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() => _DiceRollerState();
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  @override
+  var active = 'assets/images/dice-2.png';
+
+  void rollDice() {
+    var random = Random().nextInt(5) + 1;
+    setState(() {
+      active = 'assets/images/dice-$random.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          active,
+          width: 100,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            // padding: const EdgeInsets.only(
+            //   top: 20,
+            // ),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
